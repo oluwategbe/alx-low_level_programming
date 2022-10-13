befa2ef6f4ab6d4498695dd1f197e8e5366c9f51
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-10-13 08:08:55 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
 #include <unistd.h>
 /**
- *
- *  * main - entry point
- *
- *   * Return: Always 1
- *
- *    */
+ * main - entry point
+ * Return: Always 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
