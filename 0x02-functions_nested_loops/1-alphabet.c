67a3c75baaca8618a0c1ed89be49a6d3aa907db1
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-10-18 13:32:15 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowecase, then a new line
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int start;
+
+	for (start = 'a'; start < 'z'; start++)
+	{
+		_putchar(start);
+	}
+	putchar('\n');
+}
