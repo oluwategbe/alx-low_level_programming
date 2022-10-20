dd8276eabc88ddc766be3be67b696afd3a3e24cb
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-10-20 00:05:27 -0700

    first project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests the positive_or_negative function
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
