92526108b1dfed899a4489f58b06e154733e1abb
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-10-26 13:31:21 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	while (src[i] != '\0')
-	{
-		for (i = 0; i < n; i++)
-		{
-			dest[i] = src[i];
-		}
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 	for ( ; i < n; i++)
-	{
 		dest[i] = '\0';
-	}
+
 	return (dest);
 }
