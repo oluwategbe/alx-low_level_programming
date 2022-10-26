e2112c4e9e6f0d633a5abd74ed85f6559b649a34
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-10-26 05:35:50 -0700

    done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int i, j, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
